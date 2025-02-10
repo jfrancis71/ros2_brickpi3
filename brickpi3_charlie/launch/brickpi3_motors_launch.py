b13a7a38a4d055373f509fc6b9597aa9b7633b08
Julian Francis <julian.w.francis@gmail.com>
2025-02-10 15:36:40 +0000

    Adding ROS2 Jazzy port.

--- brickpi3_charlie/launch/brickpi3_motors_launch.py
+++ brickpi3_charlie/launch/brickpi3_motors_launch.py
@@ -10,22 +10,40 @@ def generate_launch_description():
         PathJoinSubstitution(
             [FindPackageShare("brickpi3_charlie"), "config", "robot_hardware_description.urdf"])
         ])
+
     robot_controllers = PathJoinSubstitution(
             [FindPackageShare("brickpi3_charlie"), "config", "robot_description.yaml"])
+
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{"robot_description": robot_description_content}, robot_controllers],
-        remappings=[("/differential_drive_controller/cmd_vel_unstamped", "/cmd_vel")],
+        parameters=[robot_controllers],
         output="both",
     )
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["differential_drive_controller", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "differential_drive_controller",
+            "--param-file",
+            robot_controllers,
+            "--controller-ros-args",
+            "-r /differential_drive_controller/cmd_vel:=/cmd_vel",
+        ],
     )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
     nodes = [
         control_node,
+        robot_state_pub_node,
         robot_controller_spawner
     ]
 
